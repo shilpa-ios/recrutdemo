d51ddfc32c952766d0f1f45715d01ccaea2f3835
Shilpa S <shilpa.s@digital.datamatics.com>
2021-10-25 20:27:21 +0530

    Update ImageProvider.swift
    Refactored the code for changes in Image Cache.

--- RecrutDemo/Classes/Networking/ImageProvider.swift
+++ RecrutDemo/Classes/Networking/ImageProvider.swift
@@ -6,11 +6,12 @@ typealias DownloadCompletion = ((_ image: UIImage?, _ urlString: String) -> ())?
 class ImageProvider {
     
     private let networkLayer: NetworkLayer
-    private let queue = DispatchQueue(label: "imageDownloading")
+    private let queue = DispatchQueue(label: "imageDownloading",attributes: .concurrent)
     private let cache = ImageCache()
     
     init() {
         self.networkLayer = NetworkLayer.sharedInstance
+    
     }
     
     func imageAsync(from urlString: String, completion: DownloadCompletion) {
@@ -20,11 +21,16 @@ class ImageProvider {
             completion?(nil, urlString)
             return
         }
+        //Check in cache if the image is available, if YES return the image.
+        if let image = self.cache.image(for: url) {
+            DispatchQueue.main.async { () -> Void in
+                completion?(image, urlString)
+            }
+            return
+        }
         
-        var imageName = self.imageName(for: url)
         queue.async { [weak self] in
-            
-            self?.downloadImage(from: url, saveAs: imageName, completion: { (image, urlString) in
+            self?.downloadImage(from: url, completion: { (image, urlString) in
                 
                 DispatchQueue.main.async { () -> Void in
                     completion?(image, urlString)
@@ -33,31 +39,57 @@ class ImageProvider {
         }
     }
 
-    private func downloadImage(from url: URL, saveAs imageName: String, completion: DownloadCompletion) {
-        
-        networkLayer.downloadFile(from: url, completion: { (locationURL, response, error) in
+//    SS: Everytime the image is downloaded from URL, though there is a provision to save it locally it is never fetch from there. This method should first check if the image is available locally, if not check in application cache. If image is not available then it should fetch it from url.
+    private func downloadImage(from url: URL, completion: DownloadCompletion) {
+        networkLayer.downloadImage(from: url, completion: {
+            (data, response, error) in
             
             let urlString = url.absoluteString
-            guard let location = locationURL else {
-                
-                completion?(nil, urlString)
+            guard let imageData = data else {
+                completion?(nil,urlString)
                 return
             }
             
-            let name = self.cache.imageName(for: url)
-            let image = self.cache.storeImageInCache(from: location, imageName: name)
-            completion?(image, urlString)
+            DispatchQueue.main.async {
+                
+                guard let image = UIImage(data: imageData) else {
+                    completion?(nil,urlString)
+                    return
+                }
+                
+                let name = self.cache.imageName(for: url)
+                self.cache.store(image: image, imageName: name)
+                completion?(image,urlString)
+
+            }
         })
-    }
-    
-    func imageName(for url: URL) -> String {
         
-        var imageName = url.lastPathComponent
-        if let size = url.query {
-            imageName = imageName + size
-        }
-        return imageName
+//         networkLayer.downloadFile(from: url, completion: { (locationURL, response, error) in
+//
+//            let urlString = url.absoluteString
+//            guard let location = locationURL else {
+//
+//                completion?(nil, urlString)
+//                return
+//            }
+//
+//            DispatchQueue.global().async {
+//                guard let image = self.image(from: location) else {
+//                    completion?(nil, urlString)
+//                    return
+//                }
+//
+//                DispatchQueue.main.async {
+//                    let name = self.cache.imageName(for: url)
+//                    self.cache.store(image: image, imageName: name)
+//                    completion?(image, urlString)
+//                }
+//
+//            }
+//
+//        })
     }
+        
 }
 
 
